2288a23045e70158715f422b77207f1d76a43be9
miloszKubiak <milypan69@gmail.com>
2022-08-15 16:17:02 +0200

    create user model

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("updateUser").patch(updateUser);
+router.route("/updateUser").patch(updateUser);
 
 export default router;
