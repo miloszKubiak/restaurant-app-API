074858b6587d0e30173fe50cbb7d9c2bf1e84157
miloszKubiak <milypan69@gmail.com>
2022-08-15 15:29:18 +0200

    add authController and routes

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ import connectDB from "./db/connect.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+import authRouter from "./routes/authRoutes.js";
+
 const app = express();
 dotenv.config();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Welcome");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
