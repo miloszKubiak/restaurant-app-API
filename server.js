5ad3959c2318b524dda6da095446cd9c6bb421e3
miloszKubiak <milypan69@gmail.com>
2022-08-15 14:59:15 +0200

    add two middlewares and setup db connection

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
 	res.send("Welcome");
 });
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-	console.log(`******** Server is listening on port ${port} ********`);
-});
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, () => {
+			console.log(
+				`******** Server is listening on port ${port} ********`
+			);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+start();
