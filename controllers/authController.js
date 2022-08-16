7017096b8cc09233c0a4e4e77021c06bc695ad76
miloszKubiak <milypan69@gmail.com>
2022-08-16 12:15:17 +0200

    refactor errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,17 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-
-class CustomApiError extends Error {
-	constructor(message) {
-		super(message);
-		this.statusCode = StatusCodes.BAD_REQUEST;
-	}
-}
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
 	const { name, email, password } = req.body;
 
 	if (!name || !email || !password) {
-		throw new CustomApiError("Please provide all values");
+		throw new BadRequestError("Please provide all values");
+	}
+
+	const userAlreadyExists = await User.findOne({ email });
+	if (userAlreadyExists) {
+		throw new BadRequestError("Email already in use");
 	}
 
 	const user = await User.create({ name, email, password });
