5c1c9fa1e3b3c83665847da02826181a8614ae23
miloszKubiak <milypan69@gmail.com>
2022-08-22 11:44:47 +0200

    add basic filtering

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -1,9 +1,27 @@
+import Meal from "../models/Meal.js";
+
 const getAllMealsStatic = async (req, res) => {
-	res.status(200).json({ msg: "meals testing route" });
+	const meals = await Meal.find({}).sort("-name price");
+	res.status(200).json({ nbHits: meals.length, meals });
 };
 
 const getAllMeals = async (req, res) => {
-	res.status(200).json({ msg: "meals route" });
+	const { name, category, featured } = req.query;
+
+	const queryObject = {};
+
+	if (featured) {
+		queryObject.featured = featured === "true" ? true : false;
+	}
+	if (category) {
+		queryObject.category = category;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: "i" };
+	}
+
+	const meals = await Meal.find(queryObject);
+	res.status(200).json({ nbHits: meals.length, meals });
 };
 
 export { getAllMealsStatic, getAllMeals };
