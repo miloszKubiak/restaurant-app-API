56bb4d21ca45c6d04359a1d314da35fd1f49e717
miloszKubiak <milypan69@gmail.com>
2022-08-27 13:46:41 +0200

    create unauthorized error handler and add check permissions util

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,11 @@
 import BadRequestError from "./bad-request.js";
 import NotFoundError from "./not-found.js";
 import UnAuthenticatedError from "./unauthenticated.js";
+import { UnauthorizedError } from "./unauthorized.js";
 
-export { BadRequestError, NotFoundError, UnAuthenticatedError };
+export {
+	BadRequestError,
+	NotFoundError,
+	UnAuthenticatedError,
+	UnauthorizedError,
+};
---
+++ utils/checkPermissions.js
@@ -0,0 +1,12 @@
+import CustomError from "../errors.js";
+
+const checkPermissions = (requestUser, resourceUserId) => {
+	if (requestUser.role === "admin") return;
+	if (requestUser.userId === resourceUserId.toString()) return;
+
+	throw new CustomError.UnauthorizedError(
+		"Not authorized to access this route"
+	);
+};
+
+export { checkPermissions };
